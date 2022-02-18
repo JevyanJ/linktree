3e3486db639c41cdecb32b99d7c04c6efc8831aa
Javier <javier.rodriguez@bluetab.net>
2022-02-18 23:35:25 +0100

    Style

--- src/components/linktree/LinkTree.js
+++ src/components/linktree/LinkTree.js
@@ -2,6 +2,43 @@ import React from "react";
 import avatar from "../../assets/avatar.jpeg";
 import "./style.css";
 
+const rrss = [
+  {
+    name: "Twitch",
+    link: "https://www.twitch.tv/jevyanj",
+    icon: "fa-brands fa-twitch"
+  },
+  {
+    name: "TikTok",
+    link: "https://www.tiktok.com/@jevyanj",
+    icon: "fab fa-tiktok"
+  },
+  {
+    name: "Twitter",
+    link: "https://twitter.com/JevyanJ",
+    icon: "fab fa-twitter"
+  },
+  {
+    name: "YouTube",
+    link: "https://www.youtube.com/channel/UCAiSqHx1xu5IHcz7M4GJGDg",
+    icon: "fab fa-youtube"
+  },
+  {
+    name: "Instagram",
+    link: "https://www.instagram.com/jevyanj/",
+    icon: "fab fa-instagram"
+  }
+];
+
+function rrss_button(element) {
+  return (
+    <a href={element["link"]} target="_blank" class="link-items">
+      <i class={element["icon"]}></i>
+      <div>{element["name"]}</div>
+    </a>
+  );
+}
+
 export default function LinkTree() {
   return (
     <div>
@@ -17,48 +54,7 @@ export default function LinkTree() {
         >
           @JevyanJ
         </a>
-        <div class="links">
-          <a
-            href="https://www.twitch.tv/jevyanj"
-            target="_blank"
-            class="link-items"
-          >
-            <i class="fa-brands fa-twitch"></i>
-            <div>Twitch</div>
-          </a>
-          <a
-            href="https://www.tiktok.com/@jevyanj"
-            target="_blank"
-            class="link-items"
-          >
-            <i class="fab fa-tiktok"></i>
-            <div>TikTok</div>
-          </a>
-          <a
-            href="https://twitter.com/JevyanJ"
-            target="_blank"
-            class="link-items"
-          >
-            <i class="fab fa-twitter"></i>
-            <div>Twitter</div>
-          </a>
-          <a
-            href="https://www.youtube.com/channel/UCAiSqHx1xu5IHcz7M4GJGDg"
-            target="_blank"
-            class="link-items"
-          >
-            <i class="fab fa-youtube"></i>
-            <div>YouTube</div>
-          </a>
-          <a
-            href="https://www.instagram.com/jevyanj/"
-            target="_blank"
-            class="link-items"
-          >
-            <i class="fab fa-instagram"></i>
-            <div>Instagram</div>
-          </a>
-        </div>
+        <div class="links">{rrss.map(rrss_button)}</div>
       </div>
     </div>
   );
